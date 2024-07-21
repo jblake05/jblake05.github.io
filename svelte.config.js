a8edb1647beb5063788e289c029fa8f9a3144cff
Jeff Blake <jtblake@middlebury.edu>
2024-07-21 19:10:39 -0400

    Changed gitignore to ignore build, added vite back to preprocessing

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
-
+	preprocess: [
+		sveltePreprocess(),
+		vitePreprocess
+	],
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
